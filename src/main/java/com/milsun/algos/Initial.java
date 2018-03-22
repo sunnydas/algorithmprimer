fd39a36bd452d4269a167c23eebd009288fd5aba
Sunny Das <das.sunny@gmail.com>
2018-03-22 18:15:13 +0530

    Adding initial src directory.

---
+++ src/main/java/com/milsun/algos/Initial.java
@@ -0,0 +1,15 @@
+package com.milsun.algos;
+
+/**
+ * Created by sundas on 3/22/2018.
+ */
+public class Initial {
+
+  /**
+   *
+   * @param args
+   */
+  public static void main(String[] args) {
+    System.out.println("Let us begin!");
+  }
+}
