7ed771012d6c2e9c1cbbaafbb17e7a1bc34a52be
Sunny Das <das.sunny@gmail.com>
2018-03-28 16:23:55 +0530

    Adding brute force implementation of 0/1 Knasack problem.

--- src/main/java/com/milsun/algos/dp/ZeroOneKnapSackProblem.java
+++ src/main/java/com/milsun/algos/dp/ZeroOneKnapSackProblem.java
@@ -21,17 +21,40 @@ public class ZeroOneKnapSackProblem {
    * @param capacity
    * @return
    */
+  public static int knapSackBruteForce(int[] weights,int[] values,int capacity,int n){
+    if(n == 0 || capacity == 0){
+      return 0;
+    }
+    else if(weights[n] > capacity){
+      return knapSackBruteForce(weights,values,capacity,n-1);
+    }
+    else{
+      int tmp1 = knapSackBruteForce(weights,values,capacity,n-1);
+      int tmp2 = values[n] + knapSackBruteForce(weights,values,capacity - weights[n],n-1);
+      return Math.max(tmp1,tmp2);
+    }
+  }
+
+  /**
+   *
+   * @param weights
+   * @param values
+   * @param capacity
+   * @return
+   */
   public static int knapSack(int[] weights,int[] values,int capacity){
     int max = Integer.MIN_VALUE;
     int[][] itemMatrix = new int[values.length][capacity+1];
-    //boolean[][] pathMatrix = new boolean[values.length][capacity+1];
+    boolean[][] pathMatrix = new boolean[values.length][capacity+1];
     for(int i = 0 ; i < itemMatrix.length ; i++){
       for(int j = 1 ; j < itemMatrix[i].length ; j++){
         if((i-1) < 0){
           itemMatrix[i][j] = values[i];
+          pathMatrix[i][j] = true;
         }
         else if(weights[i] > j){
           itemMatrix[i][j] = itemMatrix[i-1][j];
+          pathMatrix[i][j] = false;
         }
         else{
           int includeValue = values[i];
@@ -39,37 +62,41 @@ public class ZeroOneKnapSackProblem {
             includeValue += itemMatrix[i-1][j - weights[i]];
           }
           itemMatrix[i][j] = Math.max(includeValue,itemMatrix[i-1][j]);
+          if(includeValue >= itemMatrix[i-1][j]){
+            pathMatrix[i][j] = true;
+          }
+          else{
+            pathMatrix[i][j] = false;
+          }
         }
       }
     }
     print2DMatrix(itemMatrix);
-    max = itemMatrix[values.length - 1][capacity];
-    //printPath(pathMatrix,weights,capacity);
-    /*max = itemMatrix[values.length - 1][capacity];
-    int i = itemMatrix.length - 1;
-    int j = itemMatrix[i].length - 1;
-    System.out.println("Path:");
+    System.out.println();
+    print2DMatrix(pathMatrix);
+    System.out.println();
     int runCapacity = capacity;
-    while(i >= 0 && j >=0){
-      if(i-1 < 0){
-        System.out.println("item = " + i + " weight = " + weights[i] + " values = " + values[i]);
-        i = i - 1;
+    for(int i = itemMatrix.length - 1,j = itemMatrix[i].length - 1 ; i >= 0 && j >= 0 ; i--){
+      if(pathMatrix[i][j]){
+        System.out.println(i + " " + j);
+        int diff = weights[i];
+        j = runCapacity - diff;
+        runCapacity = runCapacity - diff;
       }
-      else if(itemMatrix[i][j] != itemMatrix[i-1][j]){
-        System.out.println(" item = " + i + " weight = " + weights[i] + " value " + values[i]);
-        j = runCapacity - weights[i];
-        runCapacity = runCapacity - weights[i];
-        i = i - 1;
-        //System.out.println(j);
-      }
-      else{
-        i = i - 1;
-      }
-    }*/
+    }
+    max = itemMatrix[values.length - 1][capacity];
     return max;
   }
 
 
+  public static void print2DMatrix(boolean[][] input){
+    for(int i = 0; i < input.length ; i++){
+      for(int j = 0 ; j < input[i].length ; j++){
+        System.out.print(input[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
 
   public static void print2DMatrix(int[][] input){
     for(int i = 0; i < input.length ; i++){
@@ -92,8 +119,13 @@ public class ZeroOneKnapSackProblem {
     int max = knapSack(weights,values,capacity);
     System.out.println(" max " + max);
     //System.out.println(getItemsInOptimalSolution());
+    /*
+    -1 represents padding
+     */
+    weights = new int[]{-1,1,3,4,5};
+    values = new int[]{-1,1,4,5,7};
+    System.out.println("max via brute force = " + knapSackBruteForce(weights,values,capacity,weights.length - 1));
   }
-
 
 
 }
